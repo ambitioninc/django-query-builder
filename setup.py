bccf162a8d20e47eb276990eea653cc00ec3abd6
Wes Okes <wes.okes@akimbo.io>
2013-01-24 17:08:41 -0500

    * Added basic setup info

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = "querybuilder",
+    version = "0.1",
+    packages = [
+        "querybuilder",
+    ],
+    url = "https://github.com/wesokes/django-query-builder",
+    description = "Django query builder",
+    install_requires = [
+        "django>=1.4",
+    ]
+)
+
