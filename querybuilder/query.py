d542ee14528a5aed323b4382d0e14751977872ff
Wes Okes <wes.okes@akimbo.io>
2013-02-14 15:33:23 -0500

    * Moved query window class to window functions file

--- querybuilder/query.py
+++ querybuilder/query.py
@@ -689,25 +689,3 @@ class Query(object):
                 inner_queries.append(inner_query)
                 inner_queries += inner_query['table'].get_inner_queries()
         return inner_queries
-
-
-class QueryWindow(Query):
-
-    def partition_by(self, group):
-        return super(QueryWindow, self).group_by(group)
-
-    def get_query(self):
-        """
-        @return: self
-        """
-        query = self.build_partition_by_fields()
-        query += self.build_order()
-        query += self.build_limit()
-        return query
-
-    def build_partition_by_fields(self):
-        """
-        @return: str
-        """
-        select_sql = super(QueryWindow, self).build_groups()
-        return select_sql.replace('GROUP BY', 'PARTITION BY', 1)
--- querybuilder/window_functions.py
+++ querybuilder/window_functions.py
@@ -1,3 +1,6 @@
+from querybuilder.query import Query
+
+
 class WindowFunction(object):
     name = ''
 
@@ -11,3 +14,24 @@ class WindowFunction(object):
 
 class Rank(WindowFunction):
     name = 'rank'
+
+
+class QueryWindow(Query):
+    def partition_by(self, group):
+        return super(QueryWindow, self).group_by(group)
+
+    def get_query(self):
+        """
+        @return: self
+        """
+        query = self.build_partition_by_fields()
+        query += self.build_order()
+        query += self.build_limit()
+        return query
+
+    def build_partition_by_fields(self):
+        """
+        @return: str
+        """
+        select_sql = super(QueryWindow, self).build_groups()
+        return select_sql.replace('GROUP BY', 'PARTITION BY', 1)
