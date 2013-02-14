7994f44a4209d47de1d7cbe0076d0f9fa542ae73
Wes Okes <wes.okes@akimbo.io>
2013-02-14 14:11:16 -0500

    * Added test project files for testing

---
+++ querybuilder/tests.py
@@ -0,0 +1,79 @@
+from django.utils import unittest
+
+
+class TestSelect(unittest.TestCase):
+
+    def test_select_all_from_string(self):
+        raise NotImplementedError
+
+    def test_select_all_from_string_alias(self):
+        raise NotImplementedError
+
+    def test_select_all_from_model(self):
+        raise NotImplementedError
+
+    def test_select_all_from_model_alias(self):
+        raise NotImplementedError
+
+    def test_select_fields_from_string(self):
+        raise NotImplementedError
+
+    def test_select_fields_from_string_alias(self):
+        raise NotImplementedError
+
+    def test_select_fields_from_model(self):
+        raise NotImplementedError
+
+    def test_select_fields_alias_from_model_alias(self):
+        raise NotImplementedError
+
+    def test_select_fields_alias_from_string(self):
+        raise NotImplementedError
+
+    def test_select_fields_alias_from_string_alias(self):
+        raise NotImplementedError
+
+    def test_select_fields_alias_from_model(self):
+        raise NotImplementedError
+
+    def test_select_fields_alias_from_model_alias(self):
+        raise NotImplementedError
+
+
+class TestOrderBy(unittest.TestCase):
+
+    def test_order_by_single_asc(self):
+        raise NotImplementedError
+
+    def test_order_by_list_asc(self):
+        raise NotImplementedError
+
+    def test_order_by_args_asc(self):
+        raise NotImplementedError
+
+    def test_order_by_single_desc(self):
+        raise NotImplementedError
+
+    def test_order_by_list_desc(self):
+        raise NotImplementedError
+
+    def test_order_by_args_desc(self):
+        raise NotImplementedError
+
+    def test_order_by_chained(self):
+        raise NotImplementedError
+
+
+class TestLimit(unittest.TestCase):
+
+    def test_limit(self):
+        raise NotImplementedError
+
+    def test_offset(self):
+        raise NotImplementedError
+
+    def test_limit_with_offset(self):
+        raise NotImplementedError
+
+
+
