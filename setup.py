f0f62bac487c8672b1d81861f8c1f8c9a8daa76b
Wes Okes <wes.okes@gmail.com>
2013-12-04 10:11:06 -0500

    * lowered django requirement and bumped to 0.5.2

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name="django-query-builder",
-    version="0.5.1",
+    version="0.5.2",
     packages=[
         'querybuilder'
     ],
     url="https://github.com/wesokes/django-query-builder",
     description="Django query builder",
     install_requires=[
-        "django>=1.6",
+        "django>=1.4",
         "pytz==2012h"
     ]
 )
